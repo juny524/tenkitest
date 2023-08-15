8278cf74327e4562fdbb6ab34ba057b3e013aae3
juny524 <juny5241@gmail.com>
2023-08-15 09:37:20 +0900

    お天気APIから奈良県のお天気情報を取得してファイルに保存する

---
+++ index.js
@@ -0,0 +1,11 @@
+var request = require('request');
+var fs = require('fs');
+var options = {
+  url: 'https://weather.tsukumijima.net/api/forecast/city/290010',
+  method: 'GET'
+}
+
+request(options, function(error, response, body){
+  var text = body;
+  fs.writeFile('hoge.txt', text, (err)=>{}); 
+})
